9923e5fec607568e601d0374c8430240b55df3be
yonatan_betito <yonatanbetito@gmail.com>
2025-08-07 15:46:19 +0300

    Add routes, controller

---
+++ controllers/complaintController.js
@@ -0,0 +1,35 @@
+import {createComplaint,getAllComplaints,} from "../modles/complaint.model.js";
+
+export async function submitComplaint(req, res) {
+  try {
+    const { category, message } = req.body;
+    await createComplaint({ category, message });
+    res.redirect("/");
+  } catch (error) {
+    console.error("eror submit:", error);
+  }
+}
+
+export async function getComplaints(req, res) {
+  try {
+    const complaints = await getAllComplaints();
+    res.json(complaints);
+  } catch (error) {
+    res.status(500).json({ error: "eror get complaints" });
+  }
+}
+
+export async function checkAdmin(req, res) {
+  const { password } = req.body;
+
+  if (password === process.env.ADMIN_PASSWORD) {
+    try {
+      const complaints = await getAllComplaints();
+      res.json({ success: true, complaints });
+    } catch (error) {
+      res.status(500).json({ success: false, message: "eror get complaint" });
+    }
+  } else {
+    res.status(403).json({ success: false, message: "not fund" });
+  }
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
 import { connect } from "./DB/connect.js";
+import complaintsRoutes from "./routes/complaints.js";
 config();
 
 const app = express();
@@ -9,11 +10,14 @@ const port = process.env.PORT || 3004;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./Public"));
+
+app.use("/", complaintsRoutes);
+
 app.listen(port, async () => {
   try {
     await connect();
-    console.log(`Server running... ${port}`);
+    console.log(`Server running on port ${port}`);
   } catch (error) {
-    console.error("Erour server:", error);
+    console.error("Error starting server:", error);
   }
 });
